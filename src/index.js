8f324e908267b88558038d5a8e563c86acfe8057
Layomi1 <107419093+Layomi1@users.noreply.github.com>
2022-06-19 20:50:29 +0100

    Update index.js
    Implemented logic for Prime function

--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
 // complete the function
-function prime (num) {
+function prime(num) {
   // code goes here
+  if (num <= 1) {
+    return false
+  }
+  for (i = 2; num > i; i++) {
+    if (num % i === 0) {
+      return false
+    }
+  }
+  return true
 }
 
-function solution (arg) {
-  return prime(arg);
+function solution(arg) {
+  return prime(arg)
 }
 
-if (typeof require !== "undefined" && require.main === module) {
+if (typeof require !== 'undefined' && require.main === module) {
   if (process.argv.length <= 2) {
-    throw Error("Require argument");
+    throw Error('Require argument')
   }
 
-  const arg = process.argv[2];
-  console.log(solution(arg));
+  const arg = process.argv[2]
+  console.log(solution(arg))
 }
 
-module.exports = { solution };
+module.exports = { solution }
